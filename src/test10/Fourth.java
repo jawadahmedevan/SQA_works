01cb970a6c07fe73a734d881e22ee4ba5b60ed89
unknown <jawadevan74@gmail.com>
2022-10-19 13:07:08 +0600

    Automation scrips

---
+++ src/test10/Fourth.java
@@ -0,0 +1,54 @@
+package test10;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Fourth extends Initiate
+{
+
+	public static void main(String[] args) throws InterruptedException
+	{
+
+		// System Property for Chrome Driver
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+
+		// Instantiate a ChromeDriver class.
+		WebDriver driver = new ChromeDriver();
+
+		// Launch Website
+		driver.navigate().to("https://ssfdemo.innovatorslab.net/app/login");
+
+		// Maximize the browser
+		// Scroll down the webpage by 5000 pixels
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("scrollBy(0, 5000)");
+
+		// Click on the Search button
+		driver.findElement(By.name("app_username")).click();
+		driver.findElement(By.name("app_username")).sendKeys("100");
+		driver.findElement(By.name("app_password")).click();
+		driver.findElement(By.name("app_password")).sendKeys("12345");
+		driver.findElement(By.cssSelector(".btn")).click();
+		driver.findElement(By.linkText("Groups")).click();
+		Thread.sleep(800);
+		driver.findElement(By.xpath("//div[3]/button")).click();
+		Thread.sleep(800);
+		driver.findElement(By.cssSelector(".customSelectInputFieldDefault")).click();
+		Thread.sleep(800);
+		driver.findElement(By.cssSelector(".customSelectDropdownItem:nth-child(2)")).click();
+		Thread.sleep(800);
+		driver.findElement(By.cssSelector(".instructionTextAreaBackground")).click();
+		Thread.sleep(800);
+		driver.findElement(By.cssSelector(".instructionTextAreaBackground")).sendKeys(groupbr);
+		Thread.sleep(800);
+		driver.findElement(By.cssSelector(".submitBtn")).click();
+		Thread.sleep(800);
+		driver.findElement(By.cssSelector(".modalDashboardOkBtn")).click();
+		Thread.sleep(1000);
+		driver.quit();
+	}
+
+}
+
---
+++ src/test10/Initiate.java
@@ -0,0 +1,124 @@
+package test10;
+
+import java.util.*;
+import java.net.MalformedURLException;
+
+public class Initiate 
+{
+	protected static String groupbr;
+	public static void main(String[] args) throws MalformedURLException, InterruptedException 
+	{
+		System.out.println("Welcome to SSF Test Automation Lab");
+		Thread.sleep(500);
+		System.out.println("What Type of testing you want to do? \n " + "1. Login Script \n " + "2. Chat Script \n "
+				+ "3. Group Creation Script \n " + "4. Bottom navigation and drawer Check \n "
+				+ "5. Instant Broadcast Test \n " + "6. SANITY TEST \n " + "7. My broadcast test \n " + "8. Dummy data insertion for testing ");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number ");
+		try 
+		{
+			int num = scanner.nextInt();
+			if (num == 1) 
+			{
+				System.out.println("Initiating login Test");
+				Thread.sleep(200);
+				try 
+				{
+					Calculator.logintest();
+				} 
+				catch (Exception e)
+				{
+					System.out.println("Login Test failed");
+				}
+			}
+			if (num == 2) 
+			{
+				try 
+				{
+					Calculator.chattest();
+				} 
+				catch (Exception e)
+				{
+					System.out.println("Chat test failed");
+				}
+				
+				
+			}
+			if (num == 3)
+			{
+				System.out.println("Initiating group creation Test");
+				Thread.sleep(200);
+				try
+				{
+					Calculator.groupcreatetest();
+				} 
+				catch (Exception e)
+				{
+					System.out.println("Group creation test failed");
+				}
+			}
+			if (num == 5)
+			{
+				System.out.println("Initiating Instant Broadcast Test");
+				Thread.sleep(200);
+				try 
+				{
+					Calculator.broadcasttest();
+				} 
+				catch (Exception e)
+				{
+					System.out.println("Instant broadcast test failed");
+				}
+			}
+			if (num == 6) 
+			{
+				System.out.println("Initiating Sanity Test");
+				Thread.sleep(200);
+				try
+				{
+				    Calculator.sanitytest();
+				}
+				catch (Exception e)
+				{
+				    System.out.println("Sanity test failed");
+				}
+			}
+			if (num == 7) 
+			{
+				
+				System.out.println("Initiating  My broadcast test");
+				Thread.sleep(200);
+				System.out.println("Mention the agent number who will be triggered by my broadcast alert (Without '@')");
+				scanner.nextLine();
+				groupbr ="@"+scanner.nextLine();
+				scanner.close();
+
+				try
+				{
+				    Calculator.mybroadcasttest();
+				}
+				catch (Exception e)
+				{
+				    System.out.println("My broadcast test failed");
+				}
+			}
+			if (num == 8) 
+			{
+				System.out.println("Initiating  Dummy data insertion");
+				Thread.sleep(200);
+				try
+				{
+				    Calculator.dummydatainsertion();
+				}
+				catch (Exception e)
+				{
+				    System.out.println("Dummy data insertion failed");
+				}
+			}
+		} 
+		catch (Exception e)
+		{
+			System.out.println("Only number please");
+		}
+	}
+}
